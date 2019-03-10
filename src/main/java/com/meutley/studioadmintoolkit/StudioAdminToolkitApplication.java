933ea64d55cb96f65ef8c13f76e3c2feb84ed1dc
Matt Utley <matthew.utley.dev@gmail.com>
2019-03-10 13:39:43 -0500

    First commit

---
+++ src/main/java/com/meutley/studioadmintoolkit/StudioAdminToolkitApplication.java
@@ -0,0 +1,13 @@
+package com.meutley.studioadmintoolkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioAdminToolkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioAdminToolkitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meutley/studioadmintoolkit/StudioAdminToolkitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.meutley.studioadmintoolkit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudioAdminToolkitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
