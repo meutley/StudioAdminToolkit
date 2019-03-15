289bf9b9611f995959efd7bb28fe9009c792a027
Matt Utley <matthew.utley.dev@gmail.com>
2019-03-15 17:01:00 -0500

    Remove studio session models

--- src/main/java/com/meutley/studioadmintoolkit/client/ClientDto.java
+++ src/main/java/com/meutley/studioadmintoolkit/client/ClientDto.java
@@ -1,14 +1,10 @@
 package com.meutley.studioadmintoolkit.client;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-
-import com.meutley.studioadmintoolkit.studiosession.StudioSessionDto;
 
 public class ClientDto implements Serializable {
 
@@ -23,8 +19,6 @@ public class ClientDto implements Serializable {
     @Email(message = "Email must be a valid e-mail address")
     private String email;
 
-    private List<StudioSessionDto> studioSessions = new ArrayList<>();
-
     public String getEmail() {
         return this.email;
     }
@@ -35,10 +29,6 @@ public class ClientDto implements Serializable {
 
     public String getName() {
         return this.name;
-    }
-
-    public List<StudioSessionDto> getStudioSessions() {
-        return this.studioSessions;
     }
 
     public void setEmail(String value) {
