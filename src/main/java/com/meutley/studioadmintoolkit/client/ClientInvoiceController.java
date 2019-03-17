be24baa6235239908ec9bd838a89eb0cb9a89e8f
Matt Utley <matthew.utley.dev@gmail.com>
2019-03-16 21:22:18 -0500

    Implement From Product functionality for invoices; minor refactoring; show formatted address in invoice forms

--- src/main/java/com/meutley/studioadmintoolkit/client/ClientInvoiceController.java
+++ src/main/java/com/meutley/studioadmintoolkit/client/ClientInvoiceController.java
@@ -1,9 +1,14 @@
 package com.meutley.studioadmintoolkit.client;
 
+import java.util.Comparator;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import com.meutley.studioadmintoolkit.invoice.InvoiceDto;
 import com.meutley.studioadmintoolkit.invoice.InvoiceService;
+import com.meutley.studioadmintoolkit.product.ProductDto;
+import com.meutley.studioadmintoolkit.product.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,19 +31,19 @@ public class ClientInvoiceController {
 
     @Autowired
     Validator validator;
-    
+
     private final ClientService clientService;
     private final InvoiceService invoiceService;
+    private final ProductService productService;
 
-    public ClientInvoiceController(
-        ClientService clientService,
-        InvoiceService invoiceService
-    ) {
+    public ClientInvoiceController(ClientService clientService, InvoiceService invoiceService,
+            ProductService productService) {
         this.clientService = clientService;
         this.invoiceService = invoiceService;
+        this.productService = productService;
     }
 
-    @GetMapping(value = {"", "/index"})
+    @GetMapping(value = { "", "/index" })
     public String index(@PathVariable int clientId, Model model) {
         ClientDto client = this.clientService.getById(clientId);
         ClientInvoiceListViewModel viewModel = new ClientInvoiceListViewModel();
@@ -62,7 +67,7 @@ public class ClientInvoiceController {
 
     @PostMapping("/create")
     public String create(@PathVariable int clientId, @Valid @ModelAttribute EditClientInvoiceViewModel viewModel,
-        final BindingResult bindingResult, final RedirectAttributes redirectAttributes) {
+            final BindingResult bindingResult, final RedirectAttributes redirectAttributes) {
 
         for (int i = 0; i < viewModel.getInvoice().getLineItems().size(); i++) {
             Object lineItem = viewModel.getInvoice().getLineItems().get(i);
@@ -74,7 +79,8 @@ public class ClientInvoiceController {
         viewModel.setClient(this.clientService.getById(clientId));
         redirectAttributes.addAttribute("clientId", clientId);
         if (bindingResult.hasErrors()) {
-            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.viewModel", bindingResult);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.viewModel",
+                    bindingResult);
             redirectAttributes.addFlashAttribute("viewModel", viewModel);
             return "redirect:/client/{clientId}/invoice/create";
         }
@@ -98,34 +104,45 @@ public class ClientInvoiceController {
 
     @PostMapping("/{id}/edit")
     public String edit(@PathVariable int clientId, @Valid @ModelAttribute EditClientInvoiceViewModel viewModel,
-        final BindingResult bindingResult, final RedirectAttributes redirectAttributes) {
+            final BindingResult bindingResult, final RedirectAttributes redirectAttributes) {
 
-            for (int i = 0; i < viewModel.getInvoice().getLineItems().size(); i++) {
-                Object lineItem = viewModel.getInvoice().getLineItems().get(i);
-                bindingResult.pushNestedPath(String.format("invoice.lineItems[%d]", i));
-                validator.validate(lineItem, bindingResult);
-                bindingResult.popNestedPath();
-            }
-    
-            viewModel.setClient(this.clientService.getById(clientId));
-            redirectAttributes.addAttribute("clientId", clientId);
-            if (bindingResult.hasErrors()) {
-                redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.viewModel", bindingResult);
-                redirectAttributes.addFlashAttribute("viewModel", viewModel);
-                redirectAttributes.addAttribute("id", viewModel.getInvoice().getId());
-                return "redirect:/client/{clientId}/invoice/{id}/edit";
-            }
-    
-            InvoiceDto invoice = viewModel.getInvoice();
-            invoice.setClient(viewModel.getClient());
-            this.invoiceService.create(invoice);
-            return "redirect:/client/{clientId}/invoice";
+        for (int i = 0; i < viewModel.getInvoice().getLineItems().size(); i++) {
+            Object lineItem = viewModel.getInvoice().getLineItems().get(i);
+            bindingResult.pushNestedPath(String.format("invoice.lineItems[%d]", i));
+            validator.validate(lineItem, bindingResult);
+            bindingResult.popNestedPath();
+        }
+
+        viewModel.setClient(this.clientService.getById(clientId));
+        redirectAttributes.addAttribute("clientId", clientId);
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.viewModel",
+                    bindingResult);
+            redirectAttributes.addFlashAttribute("viewModel", viewModel);
+            redirectAttributes.addAttribute("id", viewModel.getInvoice().getId());
+            return "redirect:/client/{clientId}/invoice/{id}/edit";
+        }
+
+        InvoiceDto invoice = viewModel.getInvoice();
+        invoice.setClient(viewModel.getClient());
+        this.invoiceService.create(invoice);
+        return "redirect:/client/{clientId}/invoice";
     }
 
     @DeleteMapping("/{id}/delete-line-item/{lineItemId}")
-    public ResponseEntity<HttpStatus> deleteLineItem(@PathVariable int clientId, @PathVariable int id, @PathVariable int lineItemId) {
+    public ResponseEntity<HttpStatus> deleteLineItem(@PathVariable int clientId, @PathVariable int id,
+            @PathVariable int lineItemId) {
         this.invoiceService.deleteLineItem(id, lineItemId);
         return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
     }
+
+    @GetMapping("/select-product-modal")
+    public String selectProductModal(Model model) {
+        List<ProductDto> products = this.productService.getAll();
+        products.sort(Comparator.comparing(ProductDto::getName));
+        model.addAttribute("products", products);
+        return "client/invoice/_select-product-modal";
+    }
+    
 
 }
--- src/main/java/com/meutley/studioadmintoolkit/client/EditClientInvoiceViewModel.java
+++ src/main/java/com/meutley/studioadmintoolkit/client/EditClientInvoiceViewModel.java
@@ -1,6 +1,10 @@
 package com.meutley.studioadmintoolkit.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.meutley.studioadmintoolkit.invoice.InvoiceDto;
+import com.meutley.studioadmintoolkit.mailingaddress.MailingAddressDto;
 
 public class EditClientInvoiceViewModel {
 
@@ -21,6 +25,30 @@ public class EditClientInvoiceViewModel {
 
     public void setInvoice(InvoiceDto invoice) {
         this.invoice = invoice;
+    }
+
+    public List<String> getMailingAddressLines() {
+        MailingAddressDto mailingAddress = this.client.getMailingAddress();
+        if (mailingAddress == null || mailingAddress.getId() == 0) {
+            return new ArrayList<>();
+        }
+
+        List<String> result = new ArrayList<>();
+        result.add(this.getClient().getName());
+        result.add(mailingAddress.getLine1());
+        if (mailingAddress.getLine2() != null && !mailingAddress.getLine2().isEmpty()) {
+            result.add(mailingAddress.getLine2());
+        }
+        if (mailingAddress.getLine3() != null && !mailingAddress.getLine3().isEmpty()) {
+            result.add(mailingAddress.getLine3());
+        }
+        result.add(
+            String.format("%s, %s %s",
+                mailingAddress.getCity(),
+                mailingAddress.getState(),
+                mailingAddress.getZipCode()));
+
+        return result;
     }
     
 }
